78e43b44bbeb3f1a2f13c324d35fdc4edd912119
Augusto Araújo <augustoaraujo4520@gmail.com>
2021-10-27 18:13:34 -0300

    heroku

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import environ
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,8 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # User Model
 
@@ -158,7 +159,7 @@ CART_ITEM_MAX_QUANTITY = 20
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
-#Mercado Pago
+# Mercado Pago
 env = environ.Env()
 env.read_env(str(BASE_DIR / ".env"))
 MERCADO_PAGO_PUBLIC_KEY = env("MERCADO_PAGO_PUBLIC_KEY")
